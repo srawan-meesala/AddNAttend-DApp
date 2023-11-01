46637e195a40e90f609377c1142d335167886530
Srawan Meesala <srawanviz@gmail.com>
2023-11-01 21:27:59 +0530

    Created a create event page (frontend part)

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -2,7 +2,37 @@ import React from 'react'
 
 const CreateEvent = () => {
   return (
-    <div>CreateEvent</div>
+    <div className='layout'>
+        <div className='head'>
+            <div className='logo'>Add<span>n</span>Attend.</div>
+            <div className='but-log'>
+                <button className='but-out'>Logout</button>
+            </div>
+        </div>
+        <div className='body'>
+            <form className='form' action="" method="post">
+                <div className="form-name fitem">
+                    <label htmlFor="name">Name Of The Event: </label>
+                    <input name="name" type="text" placeholder="Rocky's Birthday" />
+                </div>
+                <div className="form-date fitem">
+                    <label htmlFor="name">Date: </label>
+                    <input name="name" type="date" />
+                </div>
+                <div className="form-time fitem">
+                    <label htmlFor="name">Time: </label>
+                    <input name="name" type="time" />
+                </div>
+                <div className="form-location fitem">
+                    <label htmlFor="location">Location: </label>
+                    <input name="location" type="text" placeholder='New York City' />
+                </div>
+                <div className="form-submit fitem">
+                    <button className='form-but'>Create Event</button>
+                </div>
+            </form>
+        </div>
+    </div>
   )
 }
 
